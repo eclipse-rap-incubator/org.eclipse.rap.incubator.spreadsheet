d001048652bfa5faf7de4cb36ac88090b9c1b90f
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-09-19 16:09:47 +0000

    Renamed package org.eclipse.rwt in testfixture to org.eclipse.rap.rwt.testfixture to avoid split package when testfixture is turned into a bundle - bug 358115: Get rid of inter-fragment dependency between testfixture and rwt.test https://bugs.eclipse.org/bugs/show_bug.cgi?id=358115

--- tests/org.eclipse.rap.rwt.custom.test/src/org/eclipse/swt/custom/SpreadSheetLayout_Test.java
+++ tests/org.eclipse.rap.rwt.custom.test/src/org/eclipse/swt/custom/SpreadSheetLayout_Test.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 import junit.framework.TestCase;
 
-import org.eclipse.rwt.Fixture;
+import org.eclipse.rap.rwt.testfixture.Fixture;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.*;
